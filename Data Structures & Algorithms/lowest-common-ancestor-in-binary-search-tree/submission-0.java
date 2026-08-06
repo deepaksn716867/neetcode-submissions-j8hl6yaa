64c0380fd1042330ac61dc4bc39f87cf9152fd07
Deepak Srinivasan Navaneetha Krishnan <deepaksn716867@gmail.com>
2026-08-06 13:15:33 -0700

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<TreeNode> findBST(TreeNode root, TreeNode find) {
+        if(root == null) {
+            return null;
+        }
+        if(root.val == find.val) {
+            List<TreeNode> path = new ArrayList<>();
+            path.add(root);
+            return path;
+        }
+        List<TreeNode> path;
+        if(root.val > find.val) {
+            path = findBST(root.left, find);
+        } else {
+            path = findBST(root.right, find);
+        }
+        path.add(root);
+        return path;
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> pathForP = findBST(root, p);
+        List<TreeNode> pathForQ = findBST(root, q);
+         Collections.reverse(pathForP); 
+         Collections.reverse(pathForQ);
+         int i = 0, j = 0;
+         while(i < pathForP.size() && j < pathForQ.size()) {
+            if(pathForP.get(i).val == pathForQ.get(j).val) {
+                i++;
+                j++;
+                continue;
+            } else {
+                break;
+            }
+         }
+         return pathForP.get(--i);
+    }
+}
